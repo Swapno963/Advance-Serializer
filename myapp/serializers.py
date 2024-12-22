09ba8bcb0ba3b2dadd66eb0b54a9786d405661c8
swapno963 <swapno963@gmail.com>
2024-12-22 11:54:59 +0600

    added field level validation

---
+++ myapp/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Product, Order, OrderItem
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'price',
+            'stock',
+        )
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Price must be greater than 0."
+            )
+        return value
+    
+    def validate_stock(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "stock must be greater than 0."
+            )
+        return value
